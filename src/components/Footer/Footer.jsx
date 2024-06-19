9f6800c9dbf9ce3c243e0846323295dcb7bf70a0
BeneharFranco <befraalo@gmail.com>
2024-06-19 21:59:51 +0100

    Footer alineado a la derecha y Links funcionando

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,84 @@
-// import React from 'react'
-import "./Footer.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { Grid, Link } from "@mui/material";
 
-const Footer = () => {
+export default function BasicList() {
   return (
-    <footer>Footer</footer>
-  )
+    <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+      <Divider />
+      <Grid container justifyContent="flex-end">
+        <nav aria-label="main mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <GitHubIcon />
+                </ListItemIcon>
+                <Link
+                  href="https://github.com/AytamiEugRod"
+                  underline="none"
+                  color="inherit"
+                  fontFamily={"Roboto"}
+                >
+                  {"Aytami Eugenio"}
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <GitHubIcon />
+                </ListItemIcon>
+                <Link
+                  href="https://github.com/gemaencabo"
+                  underline="none"
+                  color="inherit"
+                  fontFamily={"Roboto"}
+                >
+                  {"Gema Encabo"}
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <GitHubIcon />
+                </ListItemIcon>
+                <Link
+                  href="https://github.com/yeraki"
+                  underline="none"
+                  color="inherit"
+                  fontFamily={"Roboto"}
+                >
+                  {"Yeray Sánchez"}
+                </Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <GitHubIcon />
+                </ListItemIcon>
+                <Link
+                  href="https://github.com/BeneharoFranco"
+                  underline="none"
+                  color="inherit"
+                  fontFamily={"Roboto"}
+                >
+                  {"Beneharo Franco"}
+                </Link>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+      </Grid>
+      <Divider />
+    </Box>
+  );
 }
-
-export default Footer
--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -4,7 +4,7 @@ import React from "react"
 const Home = () => {
   return (
     <div>
-      Hola Mundo!!!
+      Hello every body!!!
     </div>
   )
 }
