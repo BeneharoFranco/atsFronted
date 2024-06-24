6ae3f1889702bf6a3e0eaf51892f3d6cb42dcd81
BeneharFranco <befraalo@gmail.com>
2024-06-24 12:13:17 +0100

    Crud del servicio Company terminado

--- src/services/CompanyService.js
+++ src/services/CompanyService.js
@@ -26,5 +26,108 @@ const getAllCompany = async () => {
   }
 };
 
+const getCompany = async (id) => {
+  try {
+    const token = localStorage.getItem("token");
+    // const role = localStorage.getItem("role");
 
-export { getAllCompany };
+    const { data } = await api.get(`/company/${id}`, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+
+    if (!data || data.length === 0) {
+      console.log("Company empty or error");
+      return null;
+    }
+
+    return data;
+  } catch (error) {
+    console.error(
+      "Error fetching users:",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
+  }
+};
+
+const createCompany = async (formData) => {
+  try {
+    const token = localStorage.getItem("token");
+    // const role = localStorage.getItem("role");
+
+    const { data } = await api.post(`/company/`, formData, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+
+    if (!data || data.length === 0) {
+      console.log("Company empty or error");
+      return null;
+    }
+
+    return data;
+  } catch (error) {
+    console.error(
+      "Error fetching users:",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
+  }
+};
+
+const editCompany = async (id, formData) => {
+  try {
+    const token = localStorage.getItem("token");
+    // const role = localStorage.getItem("role");
+
+    const { data } = await api.put(`/company/${id}`, formData, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+
+    if (!data || data.length === 0) {
+      console.log("Company empty or error");
+      return null;
+    }
+
+    return data;
+  } catch (error) {
+    console.error(
+      "Error fetching users:",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
+  }
+};
+
+const deleteCompany = async (id) => {
+  try {
+    const token = localStorage.getItem("token");
+    // const role = localStorage.getItem("role");
+
+    const { data } = await api.delete(`/company/${id}`, {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+
+    if (!data || data.length === 0) {
+      console.log("Company empty or error");
+      return null;
+    }
+
+    return data;
+  } catch (error) {
+    console.error(
+      "Error fetching users:",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
+  }
+};
+
+export { getAllCompany, getCompany, createCompany, editCompany, deleteCompany };
