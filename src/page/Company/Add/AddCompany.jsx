85f2901a9b2c18a7222d8b8296bd3b578129e1c8
BeneharFranco <befraalo@gmail.com>
2024-06-26 18:48:34 +0100

    merge

---
+++ src/page/Company/Add/AddCompany.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+import "./Add.css";
+import { createCompany } from "../../../services/CompanyService";
+import { getAllCompany } from "../../../services/CompanyService";
+import Link from "@mui/material/Link";
+cd 
